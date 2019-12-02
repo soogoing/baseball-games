d4b5e4170cf76cdbafc0563c3ace9edc99ea53b1
soogoing <56636981+soogoing@users.noreply.github.com>
2019-12-02 23:32:31 +0900

    settimeout메서드를 사용하고 반복문을 추가
    시간변화에 따른 랜덤값은 변하지만 아래 누적된 결과의 오류가 있다. 해결하기 위해 반복문의 변화가 필요해보인다.

--- ballone.js
+++ ballone.js
@@ -10,37 +10,45 @@ var strikeS = document.getElementById('strikeS');
 var ballB = document.getElementById('ballB');
 var outO = document.getElementById('outO');
 
+function baseballGame() {
+    start = setInterval(function () {
 
-var random = {};
+        var random = {};
 
-random.list = '스트라이크, 볼, 아웃! 다음 타자가 타석에 입장했습니다, 안타! 다음 타자가 타석에 입장했습니다'.split(',');
+        random.list = '스트라이크, 볼, 아웃! 다음 타자가 타석에 입장했습니다, 안타! 다음 타자가 타석에 입장했습니다'.split(',');
 
-random.choice = function () {
-    var idx = Math.floor(Math.random() * this.list.length);
-    this.answer = this.list[idx];
-    this.letters = this.answer.split(',');
-    result.innerHTML = this.answer;
-};
-// random.choice();
+        random.choice = function () {
+            var idx = Math.floor(Math.random() * this.list.length);
+            this.answer = this.list[idx];
+            this.letters = this.answer.split(',');
+            result.innerHTML = this.answer;
+        };
+        random.choice();
 
-strike.innerHTML = 0;
-ball.innerHTML = 0;
-out.innerHTML = 0;
+        strike.innerHTML = 0;
+        ball.innerHTML = 0;
+        out.innerHTML = 0;
+        // var anta = 0;
 
-random.underDisplay = function () {
+        for (var i = 0; i < 3; i++) {
 
-    if (random.answer === random.list[0]) {
-        strike.innerHTML++;
-    } else if (random.answer === random.list[1]) {
-        ball.innerHTML++;
-    } else if (random.answer === random.list[2]) {
-        out.innerHTML++;
-    }
-};
-// random.underDisplay();
+            random.underDisplay = function () {
 
-var init = function () {
-    random.choice();
-    random.underDisplay();
+                if (random.answer === random.list[0]) {
+                    strike.innerHTML++;
+                } else if (random.answer === random.list[1]) {
+                    ball.innerHTML++;
+                } else if (random.answer === random.list[2]) {
+                    out.innerHTML++;
+                } else {
+                    clearInterval(start);
+                }
+            };
+            random.underDisplay();
+        }
+
+    }, 1000);
+
 }
-init();
+
+baseballGame();
