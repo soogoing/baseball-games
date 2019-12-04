06c4d2d04feb36512a82b3002e1572dc260d59ae
kimsoojin <56636981+soogoing@users.noreply.github.com>
2019-12-04 15:10:20 +0900

    주석으로 설명 추가
    어떤 동작을 위해 사용되었는지 설명 추가

--- balltwo-name.js
+++ balltwo-name.js
@@ -5,22 +5,26 @@ const form = document.querySelector(".js-teamForm"),
 const TM_LS = "currentTeam",
     SHOWING_CN = "showing";
 
+//팀이름 저장
 function saveName(text) {
     localStorage.setItem(TM_LS, text);
 }
 
-function handleSubmit(event) {
+//팀이름 제출
+function handleSubmit(event) { //제출
     event.preventDefault();
-    const currentValue = input.value;
+    const currentValue = input.value; //TEXT
     textTeamName(currentValue);
     saveName(currentValue);
 }
 
+//기본 이름 묻기
 function askForName() {
-    form.classList.add(SHOWING_CN);
+    form.classList.add(SHOWING_CN); //창에 기본 보여짐
     form.addEventListener("submit", handleSubmit);
 }
 
+//이름이 적히면 그다음 보여짐
 function textTeamName(text) {
     form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
@@ -28,7 +32,7 @@ function textTeamName(text) {
 }
 
 function loadName() {
-    const currentTeam = localStorage.getItem(TM_LS);
+    const currentTeam = localStorage.getItem(TM_LS); //로컬저장소에 저장
     if (currentTeam === null) {
         askForName();
     } else {
@@ -41,3 +45,6 @@ function init() {
 }
 
 init();
+
+//팀 이름 바꾸는 방법:
+//Console/Application/LocalStroage/file:// 에서 key delete 후 새로고침
