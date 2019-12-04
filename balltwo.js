4dc2bd56424b2a6bed4feb23b0737ebfb035cd9c
kimsoojin <56636981+soogoing@users.noreply.github.com>
2019-12-04 15:11:49 +0900

    주석으로 설명 추가
    어떤 동작을 위해 사용되었는지 설명 추가

--- balltwo.js
+++ balltwo.js
@@ -1,43 +1,47 @@
-const teamDataForm = document.querySelector("js-teamDataForm"),
+const teamDataForm = document.querySelector(".js-teamDataForm"),
     teamDataInput = teamDataForm.querySelector("input"),
-    teamDataList = document.querySelector("js-teamDataList");
+    teamDataList = document.querySelector(".js-teamDataList");
 
 const TD_LS = "teamDatas";
 
-function paintTeamData(text) {
+//입력시 리스트 보여짐
+function paintToDo(text) {
     const li = document.createElement("li");
     const span = document.createElement("span");
-    span.innerHTML = text;
+    span.innerText = text;
     li.appendChild(span);
     teamDataList.appendChild(li);
 }
 
+//팀 데이터 제출
 function handleSubmit(event) {
     event.preventDefault();
     const currentValue = teamDataInput.value;
-    paintTeamData(currentValue);
+    paintToDo(currentValue);
     teamDataInput.value = "";
 }
 
 function Datas() {
-    const teamDatas = localStorage.getItem(TD_LS);
-    if (teamDatas !== null) {
+    const teamDatas = localStorage.getItem(TD_LS); //로컬저장소에 저장
+    if (teamDatas !== null) { //form 항상 보여짐
     }
 }
 
+//메뉴창
 function openmenu(evt, menuName) {
-    var menucontent = document.getElementsByClassName("menucontent");
+    var menucontent = document.getElementsByClassName("menucontent"); //내용 불러옴
     var i;
     for (i = 0; i < menucontent.length; i++) {
-        menucontent[i].style.display = "none";
+        menucontent[i].style.display = "none"; //내용 숨김
     }
-    var menulinks = document.getElementsByClassName("menulinks");
-    for (i = 0; i < menulinks.length; i++) {
-        menulinks[i].className = menulinks[i].className.replace(" active", "");
+    var menulinks = document.getElementsByClassName("menulinks"); //메뉴탭을 불러옴
+    for (i = 0; i < menulinks.length; i++) { //
+        menulinks[i].className = menulinks[i].className.replace(" active", ""); //메뉴탭 초기화
     }
     document.getElementById(menuName).style.display = "block";
     evt.currentTarget.className += " active";
 }
+
 function init() {
     Datas();
     teamDataForm.addEventListener("submit", handleSubmit);
